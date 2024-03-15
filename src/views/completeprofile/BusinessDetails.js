f1865467845d49eb1f67839e6dd7ba532b46d5db
jAsOnN07 <jasongonsalves077@gmail.com>
2024-03-15 21:09:39 +0530

    business form changes

--- src/views/completeprofile/BusinessDetails.js
+++ src/views/completeprofile/BusinessDetails.js
@@ -14,16 +14,16 @@ import Select from '@mui/material/Select';
 export default function BusinessDetails() {
   const [age, setAge] = React.useState('');
 
-    const handleChange = (event) => {
-      setAge(event.target.value);
-    };
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   return (
     <Box sx={{ width: 1 }}>
-      <Box sx={{ width: 1, display: "flex" , marginTop:"50px" }}>
+      <Box sx={{ width: 1, display: "flex", marginTop: "50px" }}>
         <Grid container spacing={4} rowSpacing={3} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
           <Grid item xs={12}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Company-Name"
               label="Company Name"
               variant="outlined"
@@ -31,47 +31,47 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Company-Registration-Number"
               label="Company Registration No"
               variant="outlined"
             />
           </Grid>
           <Grid item xs={6}>
-          <Select
-          id="Industry"
-          sx={{width:1}}
-          value={age}
-          displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
-          onChange={handleChange}
-        >
-            <MenuItem value="">
-            <em>Industry</em>
-          </MenuItem>
-          <MenuItem value={10}>Fashion</MenuItem>
-          <MenuItem value={20}>Beauty</MenuItem>
-          <MenuItem value={30}>Electronics</MenuItem>
-          <MenuItem value={30}>Home and Furniture</MenuItem>
-          <MenuItem value={30}>Sports and Outdoor</MenuItem>
-          <MenuItem value={30}>Health and Wellness</MenuItem>
-          <MenuItem value={30}>Technology</MenuItem>
-          <MenuItem value={30}>Food and Beverage</MenuItem>
-          <MenuItem value={10}>Healthcare and Pharmaceuticals</MenuItem>
-          <MenuItem value={10}>Finance and Banking</MenuItem>
-          <MenuItem value={10}>Travel and Hospitality</MenuItem>
-          <MenuItem value={10}>Entertainment and Media</MenuItem>
-          <MenuItem value={10}>Automotive</MenuItem>
-          <MenuItem value={10}>Real Estate</MenuItem>
-          <MenuItem value={10}>Education</MenuItem>
-          <MenuItem value={10}>Non-profit and Social Causes</MenuItem>
-          <MenuItem value={10}>E-commerce</MenuItem>
-    
-        </Select>
+            <Select
+              id="Industry"
+              sx={{ width: 1 }}
+              value={age}
+              displayEmpty
+              inputProps={{ 'aria-label': 'Without label' }}
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>Industry</em>
+              </MenuItem>
+              <MenuItem value={10}>Fashion</MenuItem>
+              <MenuItem value={20}>Beauty</MenuItem>
+              <MenuItem value={30}>Electronics</MenuItem>
+              <MenuItem value={30}>Home and Furniture</MenuItem>
+              <MenuItem value={30}>Sports and Outdoor</MenuItem>
+              <MenuItem value={30}>Health and Wellness</MenuItem>
+              <MenuItem value={30}>Technology</MenuItem>
+              <MenuItem value={30}>Food and Beverage</MenuItem>
+              <MenuItem value={10}>Healthcare and Pharmaceuticals</MenuItem>
+              <MenuItem value={10}>Finance and Banking</MenuItem>
+              <MenuItem value={10}>Travel and Hospitality</MenuItem>
+              <MenuItem value={10}>Entertainment and Media</MenuItem>
+              <MenuItem value={10}>Automotive</MenuItem>
+              <MenuItem value={10}>Real Estate</MenuItem>
+              <MenuItem value={10}>Education</MenuItem>
+              <MenuItem value={10}>Non-profit and Social Causes</MenuItem>
+              <MenuItem value={10}>E-commerce</MenuItem>
+
+            </Select>
           </Grid>
           <Grid item xs={12}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Address"
               label="Address"
               variant="outlined"
@@ -79,7 +79,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Country"
               label="Country"
               variant="outlined"
@@ -87,7 +87,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="pin-code"
               label="pin code"
               variant="outlined"
@@ -95,7 +95,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={12}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Company-Description"
               label="Company Description"
               variant="outlined"
@@ -103,7 +103,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Number-of-Employees"
               label="Number of Employees"
               variant="outlined"
@@ -111,7 +111,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Annual-Revenue"
               label="Annual Revenue"
               variant="outlined"
@@ -119,7 +119,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Facebook-Username"
               label="Facebook Username"
               variant="outlined"
@@ -127,7 +127,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Instagram-Username"
               label="Instagram Username"
               variant="outlined"
@@ -135,7 +135,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={12}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Website-URL"
               label="Website URL"
               variant="outlined"
@@ -143,7 +143,7 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Annual-Revenue"
               label="Annual Revenue"
               variant="outlined"
@@ -151,15 +151,15 @@ export default function BusinessDetails() {
           </Grid>
           <Grid item xs={6}>
             <TextField
-            sx={{width:1}}
+              sx={{ width: 1 }}
               id="Annual-Revenue"
               label="Annual Revenue"
               variant="outlined"
             />
           </Grid>
 
-          
-          <Grid item xs={6}>
+
+          {/* <Grid item xs={6}>
           <Select
           id="demo-simple-select"
           sx={{width:1}}
@@ -175,8 +175,8 @@ export default function BusinessDetails() {
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
-          </Grid>
-          
+          </Grid> */}
+
         </Grid>
       </Box>
     </Box>
